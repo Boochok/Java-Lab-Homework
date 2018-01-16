1d085bf1b4dabe57823e57ffe1283bae0e54b33f
Daryya Buchok <buchokdaryya@gmail.com>
2018-01-16 23:45:35 +0300

    Range - implementation + tests

---
+++ src/main/java/com/epam/pair/programming/RangeImpl.java
@@ -0,0 +1,76 @@
+package com.epam.pair.programming;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class RangeImpl implements Range {
+    private long low;
+    private long up;
+
+    public RangeImpl(long low, long up) {
+        this.low = low;
+        this.up = up;
+    }
+
+    @Override
+    public boolean isBefore(Range otherRange) {
+        if (this.up < otherRange.getLowerBound()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean isAfter(Range otherRange) {
+        if (this.low > otherRange.getUpperBound()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean isConcurrent(Range otherRange) {
+        if (this.up == otherRange.getUpperBound() && this.low == otherRange.getLowerBound()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public long getLowerBound() {
+        return this.low;
+    }
+
+    @Override
+    public long getUpperBound() {
+        return this.up;
+    }
+
+    @Override
+    public boolean contains(long value) {
+        if (value <= this.up && value >= this.low) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    List<Long> longs = new ArrayList<>();
+    @Override
+    public List<Long> asList() {
+        for (long i = this.low; i <= this.up; i++) {
+            longs.add(i);
+        }
+        return longs;
+    }
+
+    @Override
+    public Iterator<Long> asIterator() {
+        return longs.iterator();
+    }
+}
---
+++ src/test/java/com/epam/pair/programming/RangeImplTest.java
@@ -0,0 +1,76 @@
+package com.epam.pair.programming;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class RangeImplTest {
+
+    Range testRange;
+    Range otherRange;
+    List<Long> testValues;
+
+    @BeforeEach
+    void setUp() {
+        testRange = new RangeImpl(1, 5);
+        otherRange = new RangeImpl(7, 9);
+        testValues = Arrays.asList(1L,2L,3L,4L,5L);
+    }
+
+    @Test
+    void isBefore() {
+        assertTrue(testRange.isBefore(otherRange));
+    }
+
+    @Test
+    void isAfter() {
+        assertFalse(testRange.isAfter(otherRange));
+    }
+
+    @Test
+    void isConcurrent() {
+        assertFalse(testRange.isConcurrent(otherRange));
+    }
+
+    @Test
+    void getLowerBound() {
+        assertEquals(1, testRange.getLowerBound());
+    }
+
+    @Test
+    void getUpperBound() {
+        assertEquals(5, testRange.getUpperBound());
+    }
+
+    @Test
+    void contains() {
+        assertTrue(testRange.contains(3));
+    }
+
+    @Test
+    void asList() {
+        assertEquals(testValues, testRange.asList());
+    }
+
+    @Test
+    void asIterator() {
+        assertFalse(testRange.asIterator().hasNext());
+    }
+
+    @Test
+    void asIteratorWithInitList() {
+        testRange.asList();
+        Iterator<Long> iterator = testRange.asIterator();
+        assertTrue(iterator.hasNext());
+        while (iterator.hasNext())
+            System.out.println(iterator.next());
+    }
+
+}
