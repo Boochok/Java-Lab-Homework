a78425f525c5a03a3ea6605997c7d2276a383f8c
Daryya Buchok <buchokdaryya@gmail.com>
2018-01-18 11:52:42 +0300

    add stucture of task1

---
+++ src/main/java/com/epam/pair/programming/Task1.java
@@ -0,0 +1,16 @@
+package com.epam.pair.programming;
+
+public class Task1 {
+
+    public int factorial(int n){
+        return 0;
+    }
+
+    public int multiplication(){
+        return 0;
+    }
+
+    public int division(){
+        return 0;
+    }
+}
---
+++ src/test/java/com/epam/pair/programming/Task1Test.java
@@ -0,0 +1,25 @@
+package com.epam.pair.programming;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Task1Test {
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void factorial() {
+    }
+
+    @Test
+    void multiplication() {
+    }
+
+    @Test
+    void division() {
+    }
+
+}
